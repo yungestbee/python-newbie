37ba6d8fb0093e60a98b4c1e03afc6fbebad8c2d
yungestbee <alomatee@yahoo.com>
2025-02-14 21:22:24 +0100

    learning python

---
+++ app.py
@@ -0,0 +1,10 @@
+# Simple Calculator
+name = input("What is your name? ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print(f"Hi {name}, Check your answers below:")
+print(f"Addition: {num1 + num2}")
+print(f"Subtraction: {num1 - num2}")
+print(f"Multiplication: {num1 * num2}")
+print(f"Division: {num1 / num2}")
